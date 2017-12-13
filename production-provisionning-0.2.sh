c4941309229685a8314949f6b49cc51ee5f66f72
yas51ne <yassinemaachi5@gmail.com>
2017-12-13 22:55:29 +0100

    Update production-provisionning-0.2.sh

--- production-provisionning-0.2.sh
+++ production-provisionning-0.2.sh
@@ -8,7 +8,7 @@ echo "#                  / /___ / /_/ /_/ / (__  )   /___/ / /_/ / /_/ /_/ (__  
 echo "#                  \____/ \___/\___/ /____/          \__,_/\__/\____/____/                      #"
 echo "#                                                                                               #"
 echo "#-----------------------------------------------------------------------------------------------#"
-echo "#           For any additional informations, feel free to contact dl-all-cws@atos.net           #"
+echo "#           For any additional informations, feel free to contact yassinemaachi5@gmail.com      #"
 echo "#################################################################################################"
 
 #########installing/updating otc-tools and OpenstackClient in case of it's not installed/updated###########
@@ -19,50 +19,50 @@ sudo yum install -y otc-tools
 sudo yum  install -y  python-openstackclient
 #########################################Network###########################################################
 echo "#################################################################################################"
-echo "############################## Creating the CWS-PROD-NETWORK stack ##############################"
+echo "############################## Creating the PROD-NETWORK stack ##################################"
 echo "#################################################################################################"
-time openstack stack create -t production-network-0.2.yaml -e production-environement-0.2.yaml CWS-PROD-NETWORK --wait
+time openstack stack create -t production-network-0.2.yaml -e production-environement-0.2.yaml PROD-NETWORK --wait
 ###########################################DNS#############################################################
 echo "#################################################################################################"
-echo "############################## Creating the CWS-INTERNAL-DNS ####################################"
+echo "############################## Creating the INTERNAL-DNS ########################################"
 echo "#################################################################################################"
-dns_domain_name=cws.productuion.
-vpc_name=cws-prod-vpc
+dns_domain_name=prod.environnement.
+vpc_name=prod-vpc
 otc domain create --vpc-name $vpc_name $dns_domain_name internal-domain private
 #########################################Storage###########################################################
 echo "#################################################################################################"
-echo "############################## Creating the CWS-PROD-STORAGE stack ##############################"
+echo "############################## Creating the PROD-STORAGE stack ##################################"
 echo "#################################################################################################"
-time openstack stack create -t production-storage-0.2.yaml -e production-environement-0.2.yaml CWS-PROD-STORAGE --wait
+time openstack stack create -t production-storage-0.2.yaml -e production-environement-0.2.yaml PROD-STORAGE --wait
 #########################################Servers###########################################################
 echo "#################################################################################################"
-echo "############################## Creating the CWS-PROD-SERVERS stack ##############################"
+echo "############################## Creating the PROD-SERVERS stack ##################################"
 echo "#################################################################################################"
-time openstack stack create -t production-servers-0.2.yaml -e production-environement-0.2.yaml CWS-PROD-SERVERS --wait
+time openstack stack create -t production-servers-0.2.yaml -e production-environement-0.2.yaml PROD-SERVERS --wait
 echo "#################################################################################################"
 echo "################################### Getting Instances Names #####################################"
 echo "#################################################################################################"
 
-k8s_master_000_name=cws-prod-k8sm-000
+k8s_master_000_name=prod-k8sm-000
 echo "k8s_master_000_name: $k8s_master_000_name"
-k8s_master_001_name=cws-prod-k8sm-001
+k8s_master_001_name=prod-k8sm-001
 echo "k8s_master_001_name: $k8s_master_001_name"
-k8s_master_002_name=cws-prod-k8sm-002
+k8s_master_002_name=prod-k8sm-002
 echo "k8s_master_002_name: $k8s_master_002_name"
 
-mongodb_000_name=cws-prod-mgdb-000
+mongodb_000_name=prod-mgdb-000
 echo "mongodb_000_name: $mongodb_000_name"
-mongodb_001_name=cws-prod-mgdb-001
+mongodb_001_name=prod-mgdb-001
 echo "mongodb_001_name: $mongodb_001_name"
-mongodb_002_name=cws-prod-mgdb-002
+mongodb_002_name=prod-mgdb-002
 echo "mongodb_002_name: $mongodb_002_name"
 
-glusterfs_000_name=cws-prod-glfs-000
+glusterfs_000_name=prod-glfs-000
 echo "glusterfs_000_name $glusterfs_000_name"
-glusterfs_001_name=cws-prod-glfs-001
+glusterfs_001_name=prod-glfs-001
 echo "glusterfs_001_name $glusterfs_001_name"
 
-jumpbox_name=cws-prod-jumpbox
+jumpbox_name=prod-jumpbox
 echo "jumpbox_name: $jumpbox_name"
 
 echo "#################################################################################################"
@@ -95,26 +95,26 @@ echo "##########################################################################
 echo "################################# Getting Block Storage Names ###################################"
 echo "#################################################################################################"
 
-k8s_master_000_volume_name=cws-prod-vol-k8sm-000
+k8s_master_000_volume_name=prod-vol-k8sm-000
 echo "k8s_master_000_volume_name: $k8s_master_000_volume_name"
-k8s_master_001_volume_name=cws-prod-vol-k8sm-001
+k8s_master_001_volume_name=prod-vol-k8sm-001
 echo "k8s_master_001_volume_name: $k8s_master_001_volume_name"
-k8s_master_002_volume_name=cws-prod-vol-k8sm-002
+k8s_master_002_volume_name=prod-vol-k8sm-002
 echo "k8s_master_002_volume_name: $k8s_master_002_volume_name"
 
-mongodb_000_volume_name=cws-prod-vol-mgdb-000
+mongodb_000_volume_name=prod-vol-mgdb-000
 echo "mongodb_000_volume_name: $mongodb_000_volume_name"
-mongodb_001_volume_name=cws-prod-vol-mgdb-001
+mongodb_001_volume_name=prod-vol-mgdb-001
 echo "mongodb_001_volume_name: $mongodb_001_volume_name"
-mongodb_002_volume_name=cws-prod-vol-mgdb-002
+mongodb_002_volume_name=prod-vol-mgdb-002
 echo "mongodb_002_volume_name: $mongodb_002_volume_name"
 
-glusterfs_000_volume_name=cws-prod-vol-glfs-000
+glusterfs_000_volume_name=prod-vol-glfs-000
 echo "glusterfs_000_volume_name: $glusterfs_000_volume_name"
-glusterfs_001_volume_name=cws-prod-vol-glfs-001
+glusterfs_001_volume_name=prod-vol-glfs-001
 echo "glusterfs_001_volume_name: $glusterfs_001_volume_name"
 
-jumpbox_volume_name=cws-prod-vol-jumpbox
+jumpbox_volume_name=prod-vol-jumpbox
 echo "jumpbox_volume_name: $jumpbox_volume_name"
 
 echo "#################################################################################################"
@@ -145,7 +145,7 @@ echo "jumpbox_volume_id: $jumpbox_volume_id"
 
 ########################################Volume attachement#################################################
 echo "#################################################################################################"
-echo "######################### Creating the CWS-PROD-STORAGE-MAPPING stack ###########################"
+echo "######################### Creating the PROD-STORAGE-MAPPING stack ###########################"
 echo "#################################################################################################"
 time openstack stack create -t production-storage-mapping-0.2.yaml --parameter=\
 "\
@@ -168,11 +168,11 @@ glusterfs_000_volume_id=$glusterfs_000_volume_id;\
 glusterfs_001_volume_id=$glusterfs_001_volume_id;\
 jumpbox_volume_id=$jumpbox_volume_id\
 "\
- CWS-PROD-STORAGE-MAPPING --wait
+ PROD-STORAGE-MAPPING --wait
 echo "#################################################################################################"
 echo "##################################### All stacks output##########################################"
 echo "#################################################################################################"
-openstack stack show CWS-PROD-NETWORK
-openstack stack show CWS-PROD-STORAGE
-openstack stack show CWS-PROD-SERVERS
-openstack stack show CWS-PROD-STORAGE-MAPPING
+openstack stack show PROD-NETWORK
+openstack stack show PROD-STORAGE
+openstack stack show PROD-SERVERS
+openstack stack show PROD-STORAGE-MAPPING
